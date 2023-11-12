088bc1f726cb20fd078c88c8e02f207ceaa86c02
Klask1604 <doltuadrian420@gmail.com>
2023-11-12 17:29:21 +0200

    .

---
+++ src/components/Bars.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Bars = () => {
+  return <>123123</>;
+};
+
+export default Bars;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+
+export const navbar = () => {
+  const [activeButton, setActiveButton] = useState(null);
+  const handleButtonClick = (buttonName) => {
+    setActiveButton(buttonName);
+  };
+  return (
+    <div className="navbar">
+      <h1>ALGVIS</h1>
+      <div className="btns">
+        <button
+          style={{
+            backgroundColor:
+              activeButton === "Generate a new array" ? "#FF7043" : "#E64A19",
+            color: "white",
+          }}
+          onClick={() => handleButtonClick("Generate a new array")}
+        >
+          Generate a new array
+        </button>
+
+        <button
+          style={{
+            backgroundColor:
+              activeButton === "BubbleSort" ? "#FF7043" : "#E64A19",
+            color: "white",
+          }}
+          onClick={() => handleButtonClick("BubbleSort")}
+        >
+          BubbleSort
+        </button>
+        <button
+          style={{
+            backgroundColor:
+              activeButton === "Selection" ? "#FF7043" : "#E64A19",
+            color: "white",
+          }}
+          onClick={() => handleButtonClick("Selection")}
+        >
+          Selection
+        </button>
+        <button
+          style={{
+            backgroundColor:
+              activeButton === "Insertion" ? "#FF7043" : "#E64A19",
+            color: "white",
+          }}
+          onClick={() => handleButtonClick("Insertion")}
+        >
+          Insertion
+        </button>
+        <button
+          style={{
+            backgroundColor: activeButton === "Radix" ? "#FF7043" : "#E64A19",
+            color: "white",
+          }}
+          onClick={() => handleButtonClick("Radix")}
+        >
+          Radix
+        </button>
+      </div>
+    </div>
+  );
+};
+export default Navbar;
